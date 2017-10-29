b91cebf696d17b1ed541259a876f0612c9e984dd
Piotrek <piotrek120017@gmail.com>
2017-10-29 17:11:29 +0100

    3.2 punkt 1, część 2

--- src/main/java/edu/iis/powp/adapter/MyAdapter.java
+++ src/main/java/edu/iis/powp/adapter/MyAdapter.java
@@ -10,7 +10,7 @@ import edu.kis.powp.drawer.shape.LineFactory;
  * Plotter adapter to drawer with several bugs. 
  */
 public class MyAdapter extends DrawPanelController implements IPlotter
-{ 
+{
 	private int startX = 0, startY = 0;
 	
     public MyAdapter() {
@@ -32,6 +32,7 @@ public class MyAdapter extends DrawPanelController implements IPlotter
         line.setEndCoordinates(x, y);
 
 		drawLine(line);
+		this.setPosition(x, y);
     }
 
     @Override
