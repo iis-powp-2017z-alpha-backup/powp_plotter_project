e556482b6c7228d4c3941ad97cf16243b91c344a
Filip Podsiadly <fpodsiadly@gmail.com>
2017-10-29 15:24:15 +0100

    testcommit

--- src/test/java/edu/iis/powp/drawer/TestDrawer.java
+++ src/test/java/edu/iis/powp/drawer/TestDrawer.java
@@ -21,7 +21,8 @@ public class TestDrawer
         DefaultDrawerFrame.getDefaultDrawerFrame().setVisible(true);
         ILine line = LineFactory.getBasicLine();
         line.setStartCoordinates(-100, -60);
-        line.setEndCoordinates(60, 130);
+        line.setEndCoordinates(60, 130); 
+        
         controller.drawLine(line);
     }
 }
