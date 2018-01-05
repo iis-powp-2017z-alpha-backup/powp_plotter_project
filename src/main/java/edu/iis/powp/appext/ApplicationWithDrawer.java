06bf9f06b4f650455d5d64522b2208e03386a86e
Aylszen <aylszen@gmail.com>
2018-01-05 20:22:52 +0100

    3.2.1

--- src/main/java/edu/iis/powp/appext/ApplicationWithDrawer.java
+++ src/main/java/edu/iis/powp/appext/ApplicationWithDrawer.java
@@ -1,5 +1,6 @@
 package edu.iis.powp.appext;
 
+import edu.iis.powp.adapter.PlotterAdapter;
 import edu.iis.powp.app.Application;
 import edu.iis.powp.app.Context;
 import edu.iis.powp.app.DriverManager;
@@ -43,9 +44,9 @@ public class ApplicationWithDrawer {
 	private static void setupDrawerPlugin(Context context) {    
 		SelectClearPanelOptionListener selectClearPanelOptionListener = new SelectClearPanelOptionListener();
 	
-		Application.addComponent(DrawPanelController.class);
-		context.addComponentMenu(DrawPanelController.class, "Draw Panel", 0);
-		context.addComponentMenuElement(DrawPanelController.class, "Clear Panel", selectClearPanelOptionListener);
+		Application.addComponent(PlotterAdapter.class);
+		context.addComponentMenu(PlotterAdapter.class, "Draw Panel", 0);
+		context.addComponentMenuElement(PlotterAdapter.class, "Clear Panel", selectClearPanelOptionListener);
 		
         getDrawPanelController().initialize(context.getFreePanel());
 	}
@@ -56,7 +57,7 @@ public class ApplicationWithDrawer {
      * @return drawPanelController.
      */
 	public static DrawPanelController getDrawPanelController() {
-		return Application.getComponent(DrawPanelController.class);
+		return Application.getComponent(PlotterAdapter.class);
 	}
 
 }
