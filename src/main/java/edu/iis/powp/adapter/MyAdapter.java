3f4820f89ec5b06cd7e4716d10a1d19ff773e077
TotallyUniqueUsername <andrzejstrek@o2.pl>
2018-01-05 01:30:16 +0100

    punkt 3.3 dla Adaptera + porządkowanie kodu

--- src/main/java/edu/iis/powp/adapter/MyAdapter.java
+++ src/main/java/edu/iis/powp/adapter/MyAdapter.java
@@ -1,42 +1,40 @@
 package edu.iis.powp.adapter;
 
 import edu.iis.client.plottermagic.IPlotter;
+import edu.iis.powp.appext.ApplicationWithDrawer;
 import edu.kis.powp.drawer.panel.DrawPanelController;
 import edu.kis.powp.drawer.shape.ILine;
 import edu.kis.powp.drawer.shape.LineFactory;
 
-
 /**
- * Plotter adapter to drawer with several bugs. 
+ * Plotter adapter to drawer with several bugs.
  */
-public class MyAdapter extends DrawPanelController implements IPlotter
-{ 
+public class MyAdapter extends DrawPanelController implements IPlotter {
 	private int startX = 0, startY = 0;
-	
-    public MyAdapter() {
+	private DrawPanelController drawPanelController;
+
+	public MyAdapter() {
 		super();
+		this.drawPanelController = ApplicationWithDrawer.getDrawPanelController();
 	}
-    
+
 	@Override
-    public void setPosition(int x, int y)
-    {
-        this.startX = x;
-        this.startY = y;
-    }
+	public void setPosition(int x, int y) {
+		this.startX = x;
+		this.startY = y;
+	}
 
-    @Override
-    public void drawTo(int x, int y)
-    {
-        ILine line = LineFactory.getBasicLine();
-    	line.setStartCoordinates(this.startX, this.startY);
-        line.setEndCoordinates(x, y);
-        setPosition(x,y);
-		drawLine(line);
-    }
+	@Override
+	public void drawTo(int x, int y) {
+		ILine line = LineFactory.getBasicLine();
+		line.setStartCoordinates(this.startX, this.startY);
+		line.setEndCoordinates(x, y);
+		setPosition(x, y);
+		drawPanelController.drawLine(line);
+	}
 
-    @Override
-    public String toString()
-    {
-        return "@Q!$!@$!#@$(*#@&Q(%^*#@";
-    }
+	@Override
+	public String toString() {
+		return "@Q!$!@$!#@$(*#@&Q(%^*#@";
+	}
 }
--- src/main/java/edu/iis/powp/events/predefine/SelectClearPanelOptionListener.java
+++ src/main/java/edu/iis/powp/events/predefine/SelectClearPanelOptionListener.java
@@ -2,19 +2,14 @@ package edu.iis.powp.events.predefine;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import edu.iis.powp.adapter.MyAdapter;
 import edu.iis.powp.app.Application;
 import edu.kis.powp.drawer.panel.DrawPanelController;
 
+public class SelectClearPanelOptionListener implements ActionListener {
 
-public class SelectClearPanelOptionListener implements ActionListener
-{
-
-    @Override
-    public void actionPerformed(ActionEvent e)
-    {
-        DrawPanelController controller = Application.getComponent(MyAdapter.class);
-        controller.clearPanel();
-    }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		DrawPanelController controller = Application.getComponent(DrawPanelController.class);
+		controller.clearPanel();
+	}
 }
