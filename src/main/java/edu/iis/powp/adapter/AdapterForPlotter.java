aabdee7c0b2ccaf733c411dd6ea6bb23a8dc35ec
Chrumek <zaubererin@poczta.onet.pl>
2018-01-06 21:23:58 +0100

    Adapter: 3.2.1 - 3.2.3

---
+++ src/main/java/edu/iis/powp/adapter/AdapterForPlotter.java
@@ -0,0 +1,41 @@
+package edu.iis.powp.adapter;
+
+import edu.kis.powp.drawer.panel.DrawPanelController;
+import edu.iis.client.plottermagic.IPlotter;
+import edu.kis.powp.drawer.shape.LineFactory;
+import edu.kis.powp.drawer.shape.ILine;
+
+public class AdapterForPlotter implements IPlotter 
+{
+
+	private DrawPanelController drawPanelController;
+	private int startX = 0, startY = 0;
+		
+	public AdapterForPlotter(DrawPanelController dpc) 
+	{
+		this.drawPanelController=dpc;
+	}
+	    
+	@Override
+	public void setPosition(int x, int y) 
+	{
+		this.startX = x;
+		this.startY = y;
+	}
+	
+	@Override
+	public void drawTo(int x, int y)
+	{
+		ILine line = LineFactory.getBasicLine();
+		line.setStartCoordinates(this.startX, this.startY);
+		line.setEndCoordinates(x, y);
+		drawPanelController.drawLine(line);
+		this.setPosition(x, y);
+	}
+	
+	@Override
+	public String toString()
+	{
+	     return "@Q!$!@$!#@$(*#@&Q(%^*#@";
+	}
+}
--- src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
+++ src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
@@ -7,13 +7,14 @@ import java.util.logging.Logger;
 
 import edu.iis.client.plottermagic.ClientPlotter;
 import edu.iis.client.plottermagic.IPlotter;
-import edu.iis.powp.adapter.MyAdapter;
+import edu.iis.powp.adapter.AdapterForPlotter;
 import edu.iis.powp.app.Application;
 import edu.iis.powp.app.Context;
 import edu.iis.powp.app.DriverManager;
 import edu.iis.powp.appext.ApplicationWithDrawer;
 import edu.iis.powp.events.predefine.SelectChangeVisibleOptionListener;
 import edu.iis.powp.events.predefine.SelectTestFigureOptionListener;
+import edu.iis.powp.events.predefine.TestFigure02;
 import edu.kis.powp.drawer.panel.DefaultDrawerFrame;
 import edu.kis.powp.drawer.panel.DrawPanelController;
 
@@ -29,8 +30,10 @@ public class TestPlotSoftPatterns
 	 */
 	private static void setupPresetTests(Context context) {
 	    SelectTestFigureOptionListener selectTestFigureOptionListener = new SelectTestFigureOptionListener();
+		TestFigure02 testFigure02 = new TestFigure02();
 		
-		context.addTest("Figure Joe 1", selectTestFigureOptionListener);	        
+		context.addTest("Figure Joe 1", selectTestFigureOptionListener);
+		context.addTest("Figure Joe 2", testFigure02);
 	}
 
 	/**
@@ -43,7 +46,7 @@ public class TestPlotSoftPatterns
 		context.addDriver("Client Plotter", clientPlotter);
 		Application.getComponent(DriverManager.class).setCurrentPlotter(clientPlotter);
 		
-		IPlotter plotter = new MyAdapter();
+		IPlotter plotter = new AdapterForPlotter(ApplicationWithDrawer.getDrawPanelController());
 		context.addDriver("Buggy Simulator", plotter);
 
 		context.updateDriverInfo();
@@ -88,8 +91,6 @@ public class TestPlotSoftPatterns
             {
                 ApplicationWithDrawer.configureApplication();
                 Context context = Application.getComponent(Context.class);
-                
-                setupDefaultDrawerVisibilityManagement(context);
                 
             	setupDrivers(context);
             	setupPresetTests(context);
