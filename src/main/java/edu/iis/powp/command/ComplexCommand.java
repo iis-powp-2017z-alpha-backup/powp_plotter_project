2388ed8a3e2347df11d043b24ceb912e0543d1a4
Piotrek <piotrek120017@gmail.com>
2018-01-07 18:24:27 +0100

    command 3.1.4

--- src/main/java/edu/iis/powp/command/ComplexCommand.java
+++ src/main/java/edu/iis/powp/command/ComplexCommand.java
@@ -2,11 +2,16 @@ package edu.iis.powp.command;
 
 import edu.iis.client.plottermagic.IPlotter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ComplexCommand implements PlotterCommand{
 
     private List<PlotterCommand> commands;
+
+    public ComplexCommand () {
+        this.commands = new ArrayList<>();
+    }
 
     public ComplexCommand(List<PlotterCommand> commands) {
         this.commands = commands;
@@ -17,5 +22,9 @@ public class ComplexCommand implements PlotterCommand{
      for (Object command:  commands){
          ((PlotterCommand)command).execute(plotter);
      }
+    }
+
+    public void addCommand (PlotterCommand plotterCommand){
+        commands.add(plotterCommand);
     }
 }
