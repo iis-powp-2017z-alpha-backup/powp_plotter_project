e6f68d6e507c02dd290069961a2b862f6cb801c0
Radoslaw Subczynski <radoslaw.subczynski@abastra.com>
2018-01-07 13:33:45 +0100

    zadanie zakonczone

---
+++ src/main/java/edu/iis/powp/command/FactoryRectangle.java
@@ -0,0 +1,14 @@
+package edu.iis.powp.command;
+
+public class FactoryRectangle {
+
+    public ComplexCommand draw() {
+        ComplexCommand command = new ComplexCommand();
+        command.add(new CommandSetPosition(-20, -20));
+        command.add(new CommandDrawLineToPosition(-20, 60));
+        command.add(new CommandDrawLineToPosition(20, 60));
+        command.add(new CommandDrawLineToPosition(20, -20));
+        command.add(new CommandDrawLineToPosition(-20, -20));
+        return command;
+    }
+}
--- src/main/java/edu/iis/powp/events/predefine/SelectTestFigureOptionListener.java
+++ src/main/java/edu/iis/powp/events/predefine/SelectTestFigureOptionListener.java
@@ -1,18 +1,15 @@
 package edu.iis.powp.events.predefine;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import edu.iis.client.plottermagic.preset.FiguresJoe;
 import edu.iis.powp.app.Application;
 import edu.iis.powp.app.DriverManager;
 
-public class SelectTestFigureOptionListener implements ActionListener
-{
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-    @Override
-    public void actionPerformed(ActionEvent e)
-    {
+public class SelectTestFigureOptionListener implements ActionListener {
+
+    @Override public void actionPerformed(ActionEvent e) {
         FiguresJoe.figureScript1(Application.getComponent(DriverManager.class).getCurrentPlotter());
     }
 }
