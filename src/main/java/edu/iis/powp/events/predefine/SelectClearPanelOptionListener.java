090cc0786c0ab05bb33ebefaf95a9ff91eccf111
ukamiski179 <ukamiski179@gmail.com>
2018-01-07 19:00:31 +0100

    3.2 Zadanie1 part 3

--- src/main/java/edu/iis/powp/events/predefine/SelectClearPanelOptionListener.java
+++ src/main/java/edu/iis/powp/events/predefine/SelectClearPanelOptionListener.java
@@ -3,17 +3,15 @@ package edu.iis.powp.events.predefine;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import edu.iis.powp.adapter.MyAdapter;
 import edu.iis.powp.app.Application;
 import edu.kis.powp.drawer.panel.DrawPanelController;
 
-
-public class SelectClearPanelOptionListener implements ActionListener
-{
+public class SelectClearPanelOptionListener implements ActionListener {
 
     @Override
-    public void actionPerformed(ActionEvent e)
-    {
-        DrawPanelController controller = Application.getComponent(DrawPanelController.class);
+    public void actionPerformed(ActionEvent e) {
+        DrawPanelController controller = Application.getComponent(MyAdapter.class);
         controller.clearPanel();
     }
 }
--- src/main/java/edu/iis/powp/events/predefine/SelectTestFigureOptionListener.java
+++ src/main/java/edu/iis/powp/events/predefine/SelectTestFigureOptionListener.java
@@ -7,12 +7,17 @@ import edu.iis.client.plottermagic.preset.FiguresJoe;
 import edu.iis.powp.app.Application;
 import edu.iis.powp.app.DriverManager;
 
-public class SelectTestFigureOptionListener implements ActionListener
-{
+public class SelectTestFigureOptionListener implements ActionListener {
 
     @Override
-    public void actionPerformed(ActionEvent e)
-    {
-        FiguresJoe.figureScript1(Application.getComponent(DriverManager.class).getCurrentPlotter());
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "Figure Joe 1":
+                FiguresJoe.figureScript1(Application.getComponent(DriverManager.class).getCurrentPlotter());
+                break;
+            case "Figure Joe 2":
+                FiguresJoe.figureScript2(Application.getComponent(DriverManager.class).getCurrentPlotter());
+                break;
+        }
     }
 }
