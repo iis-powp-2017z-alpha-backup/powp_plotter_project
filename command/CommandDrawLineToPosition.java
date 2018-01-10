c94573d4d09ec1c9d61a539557108e3ed9817d7d
eadrwlo <wlodek14a@gmail.com>
2018-01-10 19:40:06 +0100

    Creation command class

---
+++ command/CommandDrawLineToPosition.java
@@ -0,0 +1,11 @@
+package edu.iis.powp.command;
+
+public class CommandDrawLineToPosition implements PlotterCommand {
+
+	int x, y;
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub	
+	}
+
+}
---
+++ command/CommandSetPosition.java
@@ -0,0 +1,11 @@
+package edu.iis.powp.command;
+
+public class CommandSetPosition implements PlotterCommand {
+
+	int x, y;
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub	
+	}
+
+}
