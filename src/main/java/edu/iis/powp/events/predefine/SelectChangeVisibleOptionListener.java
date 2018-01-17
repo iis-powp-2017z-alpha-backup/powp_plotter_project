20910ae511f0fdb61c0caf5b48973787393ce807
oktawianmankowski <198162@edu.p.lodz.pl>
2018-01-18 00:27:16 +0100

    adapter classes added

--- src/main/java/edu/iis/powp/events/predefine/SelectChangeVisibleOptionListener.java
+++ src/main/java/edu/iis/powp/events/predefine/SelectChangeVisibleOptionListener.java
@@ -1,30 +1,24 @@
 package edu.iis.powp.events.predefine;
 
-import java.awt.Window;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class SelectChangeVisibleOptionListener implements ActionListener
-{
-	private Window controlledWindow; 
-	
-	
+public class SelectChangeVisibleOptionListener implements ActionListener {
+
+    private Window controlledWindow;
+
     public SelectChangeVisibleOptionListener(Window controlledWindow) {
-		super();
-		this.controlledWindow = controlledWindow;
-	}
+        super();
+        this.controlledWindow = controlledWindow;
+    }
 
-
-	@Override
-    public void actionPerformed(ActionEvent e)
-    {
-        if (controlledWindow.isVisible()) 
-        {
-        	controlledWindow.setVisible(false);
-        }
-        else
-        {
-        	controlledWindow.setVisible(true);
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (controlledWindow.isVisible()) {
+            controlledWindow.setVisible(false);
+        } else {
+            controlledWindow.setVisible(true);
         }
     }
 }
