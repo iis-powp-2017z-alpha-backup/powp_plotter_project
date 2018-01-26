38b317026abafd76a417dc6d0c052967739d4cd4
oktawianmankowski <198162@edu.p.lodz.pl>
2018-01-26 05:15:36 +0100

    tests modified

--- src/test/java/edu/iis/powp/drawer/TestDrawer.java
+++ src/test/java/edu/iis/powp/drawer/TestDrawer.java
@@ -5,26 +5,14 @@ import edu.kis.powp.drawer.panel.DrawPanelController;
 import edu.kis.powp.drawer.shape.ILine;
 import edu.kis.powp.drawer.shape.LineFactory;
 
-/**
- * Drawer test.
- * 
- * @author Dominik
- */
-public class TestDrawer
-{
-   
-    
-    public static void main(String[] args)
-    {
+public class TestDrawer {
+
+    public static void main(String[] args) {
         DrawPanelController controller = new DrawPanelController();
         DefaultDrawerFrame.getDefaultDrawerFrame().setVisible(true);
         ILine line = LineFactory.getBasicLine();
-        line.setStartCoordinates(300, 100);
-        line.setEndCoordinates(60, 250);
-        ILine line1 = LineFactory.getBasicLine();
-        line1.setStartCoordinates(60, 250);
-        line1.setEndCoordinates(0, -130);
+        line.setStartCoordinates(-100, -60);
+        line.setEndCoordinates(60, 130);
         controller.drawLine(line);
-
     }
 }
--- src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
+++ src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
@@ -1,69 +1,91 @@
 package edu.iis.powp.gui;
 
-import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
+import com.sun.javafx.geom.ShapePair;
 import edu.iis.client.plottermagic.ClientPlotter;
 import edu.iis.client.plottermagic.IPlotter;
-import edu.iis.powp.adapter.MyAdapter;
+import edu.iis.powp.adapter.DrawerAdapter;
+import edu.iis.powp.adapter.LinePlotterAdapter;
 import edu.iis.powp.app.Application;
 import edu.iis.powp.app.Context;
 import edu.iis.powp.app.DriverManager;
 import edu.iis.powp.appext.ApplicationWithDrawer;
-import edu.iis.powp.events.predefine.SelectChangeVisibleOptionListener;
+import edu.iis.powp.command.ComplexCommand;
+import edu.iis.powp.command.ShapeFactory;
 import edu.iis.powp.events.predefine.SelectTestFigureOptionListener;
-import edu.kis.powp.drawer.panel.DefaultDrawerFrame;
-import edu.kis.powp.drawer.panel.DrawPanelController;
+import edu.kis.powp.drawer.shape.LineFactory;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class TestPlotSoftPatterns
-{
+public class TestPlotSoftPatterns {
+
 	private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-		
-    /**
+
+	/**
 	 * Setup test concerning preset figures in context.
-	 * 
+	 *
 	 * @param context Application context.
 	 */
 	private static void setupPresetTests(Context context) {
-	    SelectTestFigureOptionListener selectTestFigureOptionListener = new SelectTestFigureOptionListener();
-		
-		context.addTest("Figure Joe 1", selectTestFigureOptionListener);	        
+		SelectTestFigureOptionListener selectTestFigureOptionListener = new SelectTestFigureOptionListener();
+
+		context.addTest("Figure Joe 1", selectTestFigureOptionListener);
+		context.addTest("Figure Joe 2", selectTestFigureOptionListener);
+		context.addTest("Figure Jane", selectTestFigureOptionListener);
+
+		ComplexCommand rectangleCommand = ShapeFactory.getRectantgleCommands();
+		context.addTest("Rectangle", new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				rectangleCommand.execute(Application.getComponent(DriverManager.class).getCurrentPlotter());
+			}
+		});
+
+		ComplexCommand triangleCommand = ShapeFactory.getTriangleCommands();
+		context.addTest("Triangle", new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				triangleCommand.execute(Application.getComponent(DriverManager.class).getCurrentPlotter());
+			}
+		});
 	}
 
 	/**
 	 * Setup driver manager, and set default IPlotter for application.
-	 * 
+	 *
 	 * @param context Application context.
 	 */
 	private static void setupDrivers(Context context) {
 		IPlotter clientPlotter = new ClientPlotter();
 		context.addDriver("Client Plotter", clientPlotter);
 		Application.getComponent(DriverManager.class).setCurrentPlotter(clientPlotter);
-		
-		IPlotter plotter = new MyAdapter();
+
+		IPlotter plotter = new DrawerAdapter(ApplicationWithDrawer.getDrawPanelController());
 		context.addDriver("Buggy Simulator", plotter);
+
+		IPlotter linePlotter = new LinePlotterAdapter(ApplicationWithDrawer.getDrawPanelController(),
+				LineFactory.getBasicLine());
+		context.addDriver("Basic Line Simulator", linePlotter);
+
+		IPlotter dottedPlotter = new LinePlotterAdapter(ApplicationWithDrawer.getDrawPanelController(),
+				LineFactory.getDottedLine());
+		context.addDriver("Dotted Line Simulator", dottedPlotter);
+
+		IPlotter specialPlotter = new LinePlotterAdapter(ApplicationWithDrawer.getDrawPanelController(),
+				LineFactory.getSpecialLine());
+		context.addDriver("Special Line Simulator", specialPlotter);
 
 		context.updateDriverInfo();
 	}
 
 	/**
-	 * Auxiliary routines to enable using Buggy Simulator.
-	 * 
-	 * @param context Application context.
-	 */
-	private static void setupDefaultDrawerVisibilityManagement(Context context) {
-		DefaultDrawerFrame defaultDrawerWindow = DefaultDrawerFrame.getDefaultDrawerFrame();
-        context.addComponentMenuElementWithCheckBox(DrawPanelController.class, "Default Drawer Visibility", 
-        		new SelectChangeVisibleOptionListener(defaultDrawerWindow), true);
-        defaultDrawerWindow.setVisible(true);
-	}
-	
-	/**
 	 * Setup menu for adjusting logging settings.
-	 * 
+	 *
 	 * @param context Application context.
 	 */
 	private static void setupLogger(Context context) {
@@ -72,31 +94,30 @@ public class TestPlotSoftPatterns
 		context.addComponentMenuElement(Logger.class, "Clear log", (ActionEvent e) -> context.flushLoggerOutput());
 		context.addComponentMenuElement(Logger.class, "Fine level", (ActionEvent e) -> LOGGER.setLevel(Level.FINE));
 		context.addComponentMenuElement(Logger.class, "Info level", (ActionEvent e) -> LOGGER.setLevel(Level.INFO));
-		context.addComponentMenuElement(Logger.class, "Warning level", (ActionEvent e) -> LOGGER.setLevel(Level.WARNING));
+		context.addComponentMenuElement(Logger.class, "Warning level",
+				(ActionEvent e) -> LOGGER.setLevel(Level.WARNING));
 		context.addComponentMenuElement(Logger.class, "Severe level", (ActionEvent e) -> LOGGER.setLevel(Level.SEVERE));
 		context.addComponentMenuElement(Logger.class, "OFF logging", (ActionEvent e) -> LOGGER.setLevel(Level.OFF));
 	}
-		
-    /**
-     * Launch the application.
-     */
-    public static void main(String[] args)
-    {
-        EventQueue.invokeLater(new Runnable()
-        {
-            public void run()
-            {
-                ApplicationWithDrawer.configureApplication();
-                Context context = Application.getComponent(Context.class);
-                
-                setupDefaultDrawerVisibilityManagement(context);
-                
-            	setupDrivers(context);
-            	setupPresetTests(context);
-            	setupLogger(context);
-            }
 
-        });
-    }
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+
+			public void run() {
+				ApplicationWithDrawer.configureApplication();
+				Context context = Application.getComponent(Context.class);
+
+				//setupDefaultDrawerVisibilityManagement(context);
+
+				setupDrivers(context);
+				setupPresetTests(context);
+				setupLogger(context);
+			}
+
+		});
+	}
 
 }
