afdf28b75db202a44bef415c195002d0aec7b9ae
Andrzej Pawlak <andrzej_pawlak90@o2.pl>
2018-01-29 23:56:39 +0100

    3.1.1 - 3.1.2

---
+++ src/main/java/edu/iis/powp/command/CommandSetPosition.java
@@ -0,0 +1,15 @@
+package edu.iis.powp.command;
+ 
+ import edu.iis.client.plottermagic.IPlotter;
+ 
+ public class CommandSetPosition implements PlotterCommand
+ {
+ 	int x , y;
+ 
+ 	@Override
+ 	public void execute(IPlotter driver) 
+ 	{
+ 		driver.setPosition(x, y);
+ 	}
+ 
+ }
